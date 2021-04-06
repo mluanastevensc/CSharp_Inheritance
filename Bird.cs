fe284881d6b82c4bf9caa48e0ed2c9db1f6382b9
Michelle Stevens <stevens.michellec@gmail.com>
2021-04-05 20:44:05 -0700

    inheritance

---
+++ Bird.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Inheritance
+{
+    public class Bird : Animal
+    {
+        public Bird()
+        {
+            IsAlive = true;
+            LegCount = 2;
+            LandSeaAir = "Air";
+            Age = 0;
+        }
+        public string WingColor { get; set; }
+        public bool CanFly { get; set; }
+        public bool DoMigrate { get; set; }
+        public double BeakLength { get; set; }
+        
+    }
+}
